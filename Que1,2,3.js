ee882986c7e8d2b77e5ea34880f233e546812b20
Mithileshumate <104211685+Mithileshumate@users.noreply.github.com>
2022-06-30 15:09:00 +0530

    Add files via upload

---
+++ Que1,2,3.js
@@ -0,0 +1,11 @@
+//Question 1
+
+const element = document.getElementById('text');
+
+//Question 2
+ 
+const tagName = document.getElementsByTagName('h1');
+
+//Question 3
+
+const  box = document.getElementsByClassName('box');
