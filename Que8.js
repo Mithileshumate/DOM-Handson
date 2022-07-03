9c91977a239cf3163f2d63b8997d6dc5f0dcb842
Mithileshumate <104211685+Mithileshumate@users.noreply.github.com>
2022-07-03 23:25:11 +0530

    Delete Que8.js

--- Que8.js
+++
@@ -1,7 +0,0 @@
-const element = document.getElementById('heading');
-
-const btn = document.getElementById('btn');
-btn.addEventListener('click', function(){
-    element.innerText='Welcome to Elevation Academy';
-})
-    
